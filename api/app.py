45e3e6a66750d7e568481d309aa91329b0d05ae5
Abdulmunim <amunim729@gmail.com>
2023-11-16 23:42:51 +0300

    Added Edge case handling

--- api/app.py
+++ api/app.py
@@ -1,20 +1,26 @@
 from flask import Flask, request, jsonify
 from model_loader import loader, predict
 
+app = Flask(__name__)
 model, tokenizer, label_encoder = loader()
 
-app = Flask(__name__)
 
-
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict', methods=['POST'])
 def predict_host():
-    text = request.args.get('text')
-    # text = data['text']
-    prediction = str(predict(text, model, tokenizer, label_encoder))
-    print("PREDICRION: ", prediction)
-    return jsonify({'success': True, 'prediction': prediction})
-    return jsonify({'success': False, 'prediction': None})
+    try:
+        data = request.get_json()
+        if 'text' not in data:
+            raise ValueError("Missing 'text' in teh request body")
+        text = data['text']
+        prediction = predict(text, model, tokenizer, label_encoder)
+        return jsonify({'success': True, 'prediction': prediction})
+    except ValueError as ve:
+        # bad request
+        return jsonify({'success': False, 'error': str(ve)}), 400
+    except Exception as e:
+        # Internal server error
+        return jsonify({'success': False, 'error': str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
